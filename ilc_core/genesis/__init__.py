ca91a99a6877349dd9df13c9b716cfeeecceffb5
Genesis Agent <ilcops@proton.me>
2026-03-29 06:39:39 +0200

    runtime(g8): phase 481 genesis validator bootstrap runtime part 2

--- ilc_core/genesis/__init__.py
+++ ilc_core/genesis/__init__.py
@@ -17,22 +17,36 @@ from .validator_bootstrap_runtime import (
     verify_epoch_zero_state,
     verify_genesis_enrollment,
 )
+from .admission_control_bootstrap import (
+    CDL_040_DEPENDENCY,
+    GENESIS_BOOTSTRAP_PART1_DEPENDENCY,
+    GENESIS_BOOTSTRAP_PART2_VERSION,
+    build_genesis_admission_control_bundle,
+    enforce_genesis_admission,
+    verify_admission_control_bundle,
+)
 from .work_task import EpistemicWorkTask
 
 __all__ = [
+    "CDL_040_DEPENDENCY",
     "CDL_042_DEPENDENCY",
     "CDL_051_RATIFICATION_DEPENDENCY",
     "CEREMONY_REQUIRED_STEPS",
+    "GENESIS_BOOTSTRAP_PART1_DEPENDENCY",
+    "GENESIS_BOOTSTRAP_PART2_VERSION",
     "GENESIS_BOOTSTRAP_VERSION",
     "GENESIS_BUNDLE_RUNTIME_VERSION",
     "GenesisBootstrapError",
     "SCHEMA_BASELINE_DEPENDENCY",
     "GenesisBundleValidationError",
     "canonical_genesis_vectors",
+    "build_genesis_admission_control_bundle",
+    "enforce_genesis_admission",
     "generate_validator_enrollment_record",
     "generate_genesis_bundle",
     "materialize_epoch_zero_state",
     "verify_genesis_bundle",
+    "verify_admission_control_bundle",
     "verify_epoch_zero_state",
     "verify_genesis_enrollment",
     "EpistemicWorkTask",
